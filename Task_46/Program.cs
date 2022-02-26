38b99a8d27f0685f90f22db1dc6335a3a49c75ed
Anna <aanikonova15@gmail.com>
2022-02-26 16:55:26 +0300

    Написать программу масштабирования фигуры

---
+++ Task_46/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 46: Написать программу масштабирования фигуры
+
+Console.WriteLine("Сколько вершин фигуры будет введено?");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите коэффициент маштабирования");
+double k = double.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine("Введите координаты точки " + i);
+    double x = double.Parse(Console.ReadLine());
+    double y = double.Parse(Console.ReadLine());
+    Console.Write("Новые координаты точки после маштабирования " + i + " ");
+    Console.Write(" (" + (x * k));
+    Console.WriteLine("; " + (y * k) + ")");
+}
+
